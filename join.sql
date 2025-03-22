bcbd72c44c11fb0405b2de80c43fd3a47ce73c58
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-03-23 02:31:47 +0600

    updated join

--- join.sql
+++ join.sql
@@ -1,4 +1,10 @@
 select p.title, u.userName from post p join users u on p.user_id = u.id;
 
-select title,userName from post 
+select * from post 
  join users on post.user_id = users.id;
+
+select * from post
+
+--  left join 
+select * from post 
+ left join users on post.user_id = users.id;
